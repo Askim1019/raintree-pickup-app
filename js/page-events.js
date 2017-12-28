a3438cd76280221b0ec38264a49a25a5f8e7ca1e
Alex Kim <askim1019@gmail.com>
2017-12-28 19:43:47 +0000

    get a specific pin to work, renders index if invalid pin

--- js/page-events.js
+++ js/page-events.js
@@ -1,16 +1,34 @@
+function validPin() {
+	var userInput = document.getElementById('pin-input').value
+
+	var pin = 1111;
+	
+	
+	if (pin == userInput) {
+		document.getElementById("drop-off-button").onclick = function() {
+		window.location.href = "drop-off-confirm.html";
+		}
+	} else {
+		document.getElementById("drop-off-button").onclick = function() {
+		window.location.href = "index.html";
+		}	
+	}
+}
+
+
 /*---- only allows for numeric characters to be entered in for PIN ----*/
 function isNumberKey(evt) {
 	var charCode = (evt.which) ? evt.which : event.keyCode
 	if (charCode > 31 && (charCode < 48 || charCode > 57))
-	return false;
-	return true;
+		return false;
+	else
+		return true;
 }
 
 
 
-document.getElementById("drop-off-button").onclick = function() {
-	window.location.href = "drop-off-confirm.html";
-}
+
+
 
 document.getElementById("pick-up-button").onclick = function() {
 	window.location.href = "pick-up-confirm.html";
